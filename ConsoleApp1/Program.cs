6a9ed393597836a9caacecc019f9113e55e99d2b
Mehmet Topal <mehmettopal@altamira.com.tr>
2022-01-10 11:53:41 +0300

    commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,61 +1,146 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Threading;
 
 public class Program
 {
-    public static void Main()
+    //    public static void Main()
+    //    {
+    //        var people = new List<Person>()
+    //{
+    //new Person("Bill", "Smith", 41),
+    //new Person("Sarah", "Jones", 22),
+    //new Person("Stacy","Baker", 21),
+    //new Person("Vivianne","Dexter", 19 ),
+    //new Person("Bob","Smith", 49 ),
+    //new Person("Brett","Baker", 51 ),
+    //new Person("Mark","Parker", 19),
+    //new Person("Alice","Thompson", 18),
+    //new Person("Evelyn","Thompson", 58 ),
+    //new Person("Mort","Martin", 58),
+    //new Person("Eugene","deLauter", 84 ),
+    //new Person("Gail","Dawson", 19 ),
+
+    //};
+
+    //        //write linq statement for people with last name that starts with the letter D
+    //        var people1 = people.Where(x => x.LastName.StartsWith("S")).ToList();
+    //        //from c in people where c.LastName.StartsWith("D") select c;
+    //        foreach (var pp in people1)
+    //        {
+    //            Console.WriteLine("isim soyisim: {0} {1}", pp.FirstName, pp.LastName);
+    //        }
+    //        Console.WriteLine("Number of people who's last name starts with the letter D = " + people1.Count());
+
+    //        //Write linq statement for first Person Older Than 40 In Descending Alphabetical Order By First Name
+    //        var person2 = people.Where(x => x.Age > 40).OrderByDescending(x=>x.FirstName);
+    //        //from c in people where c.Age>40 orderby c.FirstName descending select c;
+
+    //        foreach (var pp in person2)
+    //        {
+    //            Console.WriteLine("isim soyisim: {0} {1}", pp.FirstName, pp.LastName);
+    //        }
+
+    //    }
+
+    //    public class Person
+    //    {
+    //        public Person(string firstName, string lastName, int age)
+    //        {
+    //            FirstName = firstName;
+    //            LastName = lastName;
+    //            Age = age;
+    //        }
+
+    //        public string FirstName { get; set; }
+    //        public string LastName { get; set; }
+    //        public int Age { get; set; }
+
+    //        //override ToString to return the person's FirstName LastName Age
+    //    }
+
+
+
+
+
+    static long totalNum = 0;
+    static void Main(string[] args)
     {
-        var people = new List<Person>()
-{
-new Person("Bill", "Smith", 41),
-new Person("Sarah", "Jones", 22),
-new Person("Stacy","Baker", 21),
-new Person("Vivianne","Dexter", 19 ),
-new Person("Bob","Smith", 49 ),
-new Person("Brett","Baker", 51 ),
-new Person("Mark","Parker", 19),
-new Person("Alice","Thompson", 18),
-new Person("Evelyn","Thompson", 58 ),
-new Person("Mort","Martin", 58),
-new Person("Eugene","deLauter", 84 ),
-new Person("Gail","Dawson", 19 ),
 
-};
+        Dictionary<char, int> keyValues = new Dictionary<char, int>();
+        keyValues.Add('H', 4);
+        keyValues.Add('e', 8);
+        keyValues.Add('l', 16);
+        keyValues.Add('o', 32);
+        keyValues.Add('W', 2);
+        keyValues.Add('r', 1);
+        keyValues.Add('d', 0);
+        string keyString = "HelloWorld";
+        var startIndex = 0;
+        var endIndex = 999999;
 
-        //write linq statement for people with last name that starts with the letter D
-        var people1 = people.Where(x => x.LastName.StartsWith("S")).ToList();
-        //from c in people where c.LastName.StartsWith("D") select c;
-        foreach (var pp in people1)
+
+
+        Stopwatch watch = new Stopwatch();
+        watch.Start();
+        Task task1 = Task.Factory.StartNew(() => Total(startIndex, endIndex - 666666, keyString, keyValues));
+        Task task2 = Task.Factory.StartNew(() => Total(startIndex + 333334, endIndex - 333333, keyString, keyValues));
+        Task task3 = Task.Factory.StartNew(() => Total(startIndex + 666664, endIndex, keyString, keyValues));
+        Task.WaitAll(task1, task2,task3);
+        watch.Stop();
+        Console.WriteLine(totalNum);
+        Console.WriteLine(watch.Elapsed.Seconds);
+    }
+
+
+
+    private static void Total(int startIndex, int endIndex, string keyString, Dictionary<char, int> keyValues)
+    {
+        long total = 0;
+
+        for (var i = startIndex; i < endIndex; i++)
         {
-            Console.WriteLine("isim soyisim: {0} {1}", pp.FirstName, pp.LastName);
+            var random = DateTime.Now.Millisecond;
+            for (var j = random; j > 0; j--)
+            {
+                var keyChar = keyString[i % 9];
+                var keyValue = keyValues[keyChar];
+                if (i % 2 == 0)
+                {
+                    total += (i * (i + 1)) / 2 * keyValue;
+                }
+                else
+                {
+                    total += (i * (i + 1)) * keyValue;
+                }
+            }
         }
-        Console.WriteLine("Number of people who's last name starts with the letter D = " + people1.Count());
+        totalNum += total;
+        //Parallel.For (startIndex, endIndex, startIndex =>
+        //{
+        //    var random = DateTime.Now.Millisecond;
+        //    for (var j = random; j > 0; j--)
+        //    {
+        //        var keyChar = keyString[startIndex % 9];
+        //        var keyValue = keyValues[keyChar];
+        //        if (startIndex % 2 == 0)
+        //        {
+        //            total += (startIndex * (startIndex + 1)) / 2 * keyValue;
+        //        }
+        //        else
+        //        {
+        //            total += (startIndex * (startIndex + 1)) * keyValue;
+        //        }
+        //    }
+        //}) ;
+        //  totalNum += total;
 
-        //Write linq statement for first Person Older Than 40 In Descending Alphabetical Order By First Name
-        var person2 = people.Where(x => x.Age > 40).OrderByDescending(x=>x.FirstName);
-        //from c in people where c.Age>40 orderby c.FirstName descending select c;
 
-        foreach (var pp in person2)
-        {
-            Console.WriteLine("isim soyisim: {0} {1}", pp.FirstName, pp.LastName);
-        }
+
 
     }
 
-    public class Person
-    {
-        public Person(string firstName, string lastName, int age)
-        {
-            FirstName = firstName;
-            LastName = lastName;
-            Age = age;
-        }
-
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public int Age { get; set; }
-
-        //override ToString to return the person's FirstName LastName Age
-    }
 }
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,15 +6,51 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data;
 using ExcelDataReader;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace ExcelConverter
 {
     class Program
     {
+
+        static /*double*/ TimeSpan CalculateWorkDurationOfPerson(List<Person> actions)
+        {
+            DateTime lastEnter = new DateTime();
+            //TimeSpan dateTime =new TimeSpan();
+            //var totalWorkDuration = 0.0;
+            TimeSpan total = new TimeSpan();
+            var lastCheckedActionType = "Out";
+            var today = new DateTime(2022, 1, 1, 0, 0, 0);  //normal şartlarda o günün tarihi baz alınacaktır. Örn:DateTime today = DateTime.Today;
+            foreach (var action in actions)
+            {
+                if(today==action.WorkTime.Date)
+                {
+                    if (action.Type == "In")
+                    {
+                        lastEnter = action.WorkTime;
+                    }
+                    else if (action.Type == "Out" && lastCheckedActionType == "In")
+                    {
+                        /*var workDuration*/
+                        TimeSpan dateTime = action.WorkTime - lastEnter;
+                        /*totalWorkDuration*/
+                        total += dateTime /*workDuration.TotalMinutes*/;
+                    }
+                    lastCheckedActionType = action.Type;
+                }
+                
+            }
+            return /*totalWorkDuration*/total;
+        }
+
+
+
         static void Main(string[] args)
         {
             //Dosyanın okunacağı dizin
-            string filePath = @"C:\Users\ALTAMIRA\Desktop\Turnike.xlsx";
+            string filePath = @"C:\Users\ALTAMIRA\Desktop\Turnike1.xlsx";
 
             //Dosyayı okuyacağımı ve gerekli izinlerin ayarlanması.
             FileStream stream = File.Open(filePath, FileMode.Open, FileAccess.Read);
@@ -51,47 +87,112 @@ namespace ExcelConverter
                 //ilk satır başlık olduğu için 2.satırdan okumaya başlıyorum.
                 if (counter > 1)
                 {
-                    liste.Add(excelReader.GetString(0)+"/"+excelReader.GetString(1)+"/"+excelReader.GetDateTime(2));
-                    
+                    liste.Add(excelReader.GetString(0) + "/" + excelReader.GetString(1) + "/" + excelReader.GetDateTime(2));
+
                 }
             }
-            
+
             //Okuma bitiriliyor.
             excelReader.Close();
             List<Person> people = new List<Person>() { };
             //Veriler ekrana basılıyor.
             foreach (var item in liste)
             {
-                
+
                 string[] _Split = item.Split('/');
                 //var parsedTime = DateTime.Parse(_Split[2]);
-                people.Add(new Person() { Name = _Split[0], Type = _Split[1] , WorkTime= DateTime.Parse(_Split[2]) });
+                people.Add(new Person() { Name = _Split[0], Type = _Split[1], WorkTime = DateTime.Parse(_Split[2]) });
 
 
 
 
             }
-            List<string> olditem = new List<string>();
-            foreach (var item in people)
+            //List<string> olditem = new List<string>();
+            //foreach (var item in people)
+            //{
+            //    if(!olditem.Contains(item.Name))
+            //    {
+            //        var kisi = people.Where(x => x.Name == item.Name).FirstOrDefault(t => t.Type == "In");
+            //        var giris = kisi.WorkTime;
+            //        var kisi1 = people.Where(x => x.Name == item.Name).LastOrDefault(t => t.Type == "Out");
+            //        var cikis = kisi1.WorkTime;
+            //        var work = cikis - giris;
+            //        Console.WriteLine(kisi.Name + " ilk giriş: " + giris + " son çıkış: " + cikis + " toplam çalışma saati: " + work);
+            //        olditem.Add(item.Name);
+            //    }
+
+
+            //}
+            //var result = people.GroupBy(s => new { s.Name,s.Type }).Select(gr => new
+            //               {
+            //                 Name = gr.Key.Name,
+            //                 Type=gr.Key.Type,
+            //                 In = gr.Min(g => g.WorkTime.TimeOfDay),
+            //                 Out=gr.Max(gr=>gr.WorkTime.TimeOfDay)
+            //}).OrderBy(x => x.Type).ToList();
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine("{0} {1} {2} {3}",item.Name,item.Type,item.In,item.Out);
+
+            //}
+
+
+
+
+            var insanlar = people.GroupBy(x => x.Name).ToList();
+            foreach (var person in insanlar)
             {
-                if(!olditem.Contains(item.Name))
-                {
-                    var kisi = people.Where(x => x.Name == item.Name).FirstOrDefault(t => t.Type == "In");
-                    var giris = kisi.WorkTime;
-                    var kisi1 = people.Where(x => x.Name == item.Name).LastOrDefault(t => t.Type == "Out");
-                    var cikis = kisi1.WorkTime;
-                    var work = cikis - giris;
-                    Console.WriteLine(kisi.Name + " ilk giriş: " + giris + " son çıkış: " + cikis + " toplam çalışma saati: " + work);
-                    olditem.Add(item.Name);
-                }
-
-                
+                Thread thread = new Thread(() =>
+                 { 
+                     var totalPersonDuration = CalculateWorkDurationOfPerson(person.ToList());
+                     Console.WriteLine(person.Key + " : " + totalPersonDuration);
+                 });
+                thread.Start();
             }
-            
+
+
+            //List<Personnel> personnel = new List<Personnel>() { };
+            //int sayac = 0;
+            //int sayacc = 0;
+            //foreach (var item in people)
+            //{
+            //    if (item.Type == "In")
+            //    {
+            //        var pers = personnel.Where(x => x.Name == item.Name).FirstOrDefault(t => t.Type == "In");
+            //        if (pers == null || sayac != 0)
+            //        {
+            //            personnel.Add(new Personnel() { Name = item.Name, Type = item.Type, WorkTime = item.WorkTime });
+            //            sayacc++;
+            //            sayac = 0;
+            //        }
+
+            //    }
+            //    else
+            //    {
+            //        var pers = personnel.Where(x => x.Name == item.Name).FirstOrDefault(t => t.Type == "Out");
+            //        if (pers == null || sayacc != 0)
+            //        {
+            //            personnel.Add(new Personnel() { Name = item.Name, Type = item.Type, WorkTime = item.WorkTime });
+            //            sayac++;
+            //            sayacc = 0;
+
+            //        }
+            //    }
+            //}
+
+            //    foreach (var itemm in personnel)
+            //    {
+            //        Console.WriteLine("{0} - {1}", itemm.Name, itemm.WorkTime);
+            //    }
+
+
+
+
+
 
             Console.ReadKey();
-            
-            
+
+
         }
     }
     public class Person
@@ -100,11 +201,29 @@ namespace ExcelConverter
         {
         }
 
-        public Person(string name, string type,DateTime workTime)
+        public Person(string name, string type, DateTime workTime)
         {
             Name = name;
             Type = type;
             WorkTime = workTime;
+        }
+
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public DateTime WorkTime { get; set; }
+
+    }
+    public class Personnel
+    {
+        public Personnel()
+        {
+        }
+
+        public Personnel(string namee, string typee, DateTime workTimee)
+        {
+            Name = namee;
+            Type = typee;
+            WorkTime = workTimee;
         }
 
         public string Name { get; set; }
